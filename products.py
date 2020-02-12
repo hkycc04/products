741c21dd16c932720fbf0a213bcc5ddb87a77553
Alan Huang <hkycc04@gmail.com>
2020-02-12 22:28:04 +0800

    檢查檔案是否存在不影響後續程式

--- products.py
+++ products.py
@@ -1,16 +1,26 @@
+import os # operation system
+
 #讀取檔案
-
-
 products = []
-with open('products.csv', 'r') as f:
-	for line in f:
-		if '商品,價格' in line:
-			continue #繼續
-		name, price = line.strip().split(',')
-		products.append([name, price])
-print(products)
+if os.path.isfile('products.csv'): #檢查檔案是否存在，但不影響後續檔案持續運作
+	print('yes. you got the file')
+	with open('products.csv', 'r') as f:
+		for line in f:
+			if '商品,價格' in line:
+				continue #繼續
+			name, price = line.strip().split(',')
+			products.append([name, price])
+	print(products)
 
 
+else:
+	print('file was not found')
+
+
+
+
+
+#輸入相關資料
 while True:
 	name = input('請輸入商品名稱: ')
 	if name == 'q':
@@ -19,10 +29,11 @@ while True:
 	products.append([name, price])
 print(products)
 
+#印出購買紀錄
 for p in products:
 	print(p[0], '售價為', p[1])
 
-
+#寫入檔案
 with open('products.csv', 'w', ) as f: #encoding='utf-8') as f:
 	f.write('商品,價格\n')
 	for p in products:
